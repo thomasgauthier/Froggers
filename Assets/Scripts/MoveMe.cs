dba72a73d1c7c0fd7f569a1c1f16b17105ff1edb
Michel DIDIER <michel.didier90@gmail.com>
2016-11-01 15:40:45 -0400

    Original Scene edit

--- Assets/Scripts/MoveMe.cs
+++ Assets/Scripts/MoveMe.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
-public class MoveMe : MonoBehaviour {
+public class MoveMe : MonoBehaviour
+{
 	public float speed;
+	private float initialSpeed;
 
-	void  FixedUpdate (){
+
+	void Awake ()
+	{
+		initialSpeed = speed;
+	}
+
+	void  FixedUpdate ()
+	{
 		transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
+	}
+
+	public void ResetSpeed ()
+	{
+		speed = initialSpeed;
 	}
 }
