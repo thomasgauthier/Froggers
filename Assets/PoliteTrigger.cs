2f1dc80e45e4244b19646a3916acdde9e325a1da
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2016-11-01 15:42:12 -0400

    polite

---
+++ Assets/PoliteTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoliteTrigger : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void  OnTriggerEnter (Collider other)
+		{
+				transform.parent.GetComponent<MoveMe> ().speed = 0;
+		}
+
+
+		void OnTriggerExit (Collider other)
+		{
+				transform.parent.GetComponent<MoveMe> ().ResetSpeed ();
+		}
+}
--- Assets/Scripts/MoveMe.cs
+++ Assets/Scripts/MoveMe.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
-public class MoveMe : MonoBehaviour {
-	public float speed;
+public class MoveMe : MonoBehaviour
+{
+		public float speed;
+		private float initialSpeed;
 
-	void  FixedUpdate (){
-		transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
-	}
+
+		void Awake ()
+		{
+				initialSpeed = speed;
+		}
+
+		void  FixedUpdate ()
+		{
+				transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
+		}
+
+		public void ResetSpeed ()
+		{
+				speed = initialSpeed;
+		}
 }
