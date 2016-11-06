7c0af585a588ca2ad93e9dedde7edb718bbf589b
Matthew Grey-Noble <mattgreynoble@gmail.com>
2016-11-06 18:02:00 -0500

    started Seinfeld audio level

---
+++ Assets/Scripts/seinfeldLoop.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class seinfeldLoop : MonoBehaviour {
+
+	CharacterController player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.velocity.magnitude > 1) {
+			GetComponent<AudioSource> ().UnPause ();
+		} else {
+			GetComponent<AudioSource> ().Pause ();
+		}
+	}
+}
