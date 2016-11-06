d2b8ec83f81ce8785aafb0a7692498f8c08a1099
Matthew Grey-Noble <mattgreynoble@gmail.com>
2016-11-06 14:41:30 -0500

    Added minimalist frogger scene + materials + start screen w/level changer script

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex) {
+		
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void QuitGame() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
---
+++ Assets/Scripts/MinimalistFrogger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MinimalistFrogger : MonoBehaviour {
+	 
+	public GameObject winMessage; //a happy face
+	private bool isShowing = false; //whether or not the happy face is showing
+	public GameObject minimalFinish; //red box
+	private bool youWin = false; //triggers win state
+
+	// Use this for initialization
+	void Start () { 
+		winMessage.SetActive (isShowing);//happy face is invisible
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//move the green sqaure along Z when the Up key is pressed
+		transform.Translate (0f, 0f, Input.GetAxis ("Vertical") * (Time.deltaTime * 2));
+
+		//if winstate is achieved, display happy face, wait 3s, return to title
+		if (youWin == true) {
+			isShowing = true; 
+			winMessage.SetActive (isShowing); 
+			Invoke ("backToStart", 3); 
+		}
+	}//end update
+		
+	void OnTriggerEnter (Collider minimalFinish) {
+		youWin = true;//trigger the win state
+	}
+
+	void backToStart() {
+		SceneManager.LoadScene (0);//return to level select screen
+	}
+		
+}
