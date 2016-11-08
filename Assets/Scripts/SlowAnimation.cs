5d14ca35aaa6861104f59618155f7b6e9fafedba
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2016-11-08 15:44:54 -0500

    fixing merge problems

---
+++ Assets/Scripts/SlowAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowAnimation : MonoBehaviour {
+
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator>().speed = speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
