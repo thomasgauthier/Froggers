cf50d5bd14585087df57e4418617909ff02b9861
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2016-11-09 15:49:25 -0500

    added president trump + fast and frogger, also fixed car sound in all scenes

---
+++ Assets/Scripts/FastManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FastManager : MonoBehaviour {
+
+	public Transform frogger;
+	public GameObject[] cars;
+	public Vector3[] startPos;
+
+	public AudioSource audioSource;
+
+	private float startZ;
+	//	public AudioClip cars;
+	bool played = false;
+	// Use this for initialization
+	void Start () {
+		startZ = frogger.position.z;
+
+		startPos = new Vector3[cars.Length];
+
+		for(int i = 0; i < cars.Length; i++)
+ {
+			startPos [i] = cars[i].transform.position;
+			}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < cars.Length; i++)
+		{
+			if (cars[i].transform.position.x > 55f) {
+				cars[i].transform.position = startPos [i];
+//				cars [i].GetComponent<MoveMe> ().enabled = false;
+//				cars [i].GetComponent<Rigidbody> ().velocity = Vector3.zero;
+//				cars[i].GetComponent<AudioSource> ().Stop ();
+
+				//played = false;
+			}	
+		}
+
+
+		if (!played) {
+
+		if (frogger.position.z > startZ) {
+			foreach (var car in cars) {
+				car.GetComponent<MoveMe> ().enabled = true;
+			}
+
+				//audioSource.Play ();
+				foreach (var car in cars) {
+					car.GetComponent<AudioSource> ().Play ();
+				}
+				played = true;
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Volume.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Volume : MonoBehaviour {
+
+	public Transform frogger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		AnimationCurve curve = GetComponent<AudioSource> ().GetCustomCurve (AudioSourceCurveType.CustomRolloff);
+
+		float distance = Mathf.Abs (transform.position.z - frogger.position.z);
+
+		float maxDistance = GetComponent<AudioSource> ().maxDistance;
+
+		GetComponent<AudioSource> ().volume = curve.Evaluate (distance / maxDistance);
+	}
+}
